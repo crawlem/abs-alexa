c0a440ae6193ede22a56773900fe9a5b693bffb4
Mark Crawley <mark@crawleyweb.co.uk>
2026-02-23 09:04:05 +0000

    Update API key and server URL in config.js

--- lambda/config.js
+++ lambda/config.js
@@ -1,7 +1,7 @@
 // config.js
 module.exports = {
-    ABS_API_KEY: 'xxxxxx',
-    SERVER_URL: 'https://abs.domain.tld',
+    ABS_API_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJrZXlJZCI6ImM0MjI4YmY3LTUzYzUtNGI4OS04ZDI1LWU3NzcyN2E2NDAzMiIsIm5hbWUiOiJhYnMtc2tpbGwiLCJ0eXBlIjoiYXBpIiwiaWF0IjoxNzcxODMyOTUyfQ.s7eLKlOZDQZyficbUfpgRVBB4OsqL2eje_yc7Owl5mk',
+    SERVER_URL: 'https://audiobooks.crawleyweb.co.uk',
     USER_AGENT: 'AlexaSkill'
     // OTHER OPTIONAL HEADERS
     // CFAccessClientId: 'xxxxxxxxx.access',
